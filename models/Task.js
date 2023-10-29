d013dea03de2daf6f01400c15e59f144ce09c586
Clement127001 <babiyonclement18@gmail.com>
2023-10-29 11:56:46 +0530

    add basic validation

--- models/Task.js
+++ models/Task.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema({
-  taskName: String,
-  completed: Boolean,
+  taskName: {
+    type: String,
+    required: [true, "Please provide the name for the task"],
+    maxlength: [30, "task name must be within 30 character"],
+    trim: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = mongoose.model("Task", TaskSchema);
